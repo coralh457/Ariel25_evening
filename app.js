ce12b1f03a97f92e6966e6ea0dfe3adea77424d3
Coral H. <coralh457@gmail.com>
2025-07-10 16:51:51 +0300

    fix: updated routes paths in app.js

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 
-const routes = require("./routes");
-const connectDB = require("./db");
-const errorHandler = require("./middleware/errorHandler");
-// const selectHandler = require("./middleware/select");
+const connectDB = require("./src/db");
+const errorHandler = require("./src/middleware/errorHandler");
+
+const toyRoutes = require("./src/routes/toys");
+const userRoutes = require("./src/routes/users");
 
 connectDB();
 
@@ -16,15 +17,14 @@ const app = express();
 
 app.use(morgan("dev"));
 app.use(cors());
-
 app.use(express.json());
 
-// app.use(selectHandler);
-
-app.use("/", routes);
+app.use("/api/toys", toyRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(errorHandler);
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server is listening on port ${process.env.PORT}!`)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>
+  console.log(`Server is listening on port ${PORT}!`)
 );
